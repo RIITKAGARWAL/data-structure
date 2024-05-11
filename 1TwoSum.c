9c2e5db4620bee204aa69d7db63239701342f116
Ritik Agarwal <110211755+RIITKAGARWAL@users.noreply.github.com>
2024-05-11 15:39:56 +0530

    Add files via upload
    1. Two Sum
    Solved
    Easy
    Topics
    Companies
    Hint
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
    You can return the answer in any order.

---
+++ 1TwoSum.c
@@ -0,0 +1,30 @@
+/**1. Two Sum
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+   
+    int i,j;
+    int *arr;
+    *returnSize = 0;
+    
+    arr = (int *)malloc(sizeof(int)*2);
+    for(i = 0; i< numsSize; i++)
+        for(j=i+1; j<numsSize; j++)
+                if(nums[i]+ nums[j]==target)
+                  { 
+                    arr[0] = i;
+                    arr[1] = j;
+                    *returnSize = 2;
+                    return arr;
+                    }
+ 
+            free(arr);
+            return NULL;
+
+}
